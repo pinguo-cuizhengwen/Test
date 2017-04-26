4ef766bac13bf886aa71b8087a2ce4bbf0ffc63a
pinguo-cuizhengwen <cuizhengwen@camera360.com>
2017-04-26 16:46:21 +0800

    add

--- src/main/script/get_command.py
+++ src/main/script/get_command.py
@@ -12,7 +12,7 @@ if not os.path.exists('lib/'):
     print 'lib folder not found, please run this command in application top directories'
 else:
     config_files = map(lambda f: str(f), filter(lambda x: str(x).endswith('.conf'), os.listdir('.')))
-    product_config_files = map(lambda f: './config/%s/%s' % (args.pid, f), filter(lambda x: str(x).endswith('.conf'), os.listdir('./config/%s' % args.pid)))
+    #product_config_files = map(lambda f: './config/%s/%s' % (args.pid, f), filter(lambda x: str(x).endswith('.conf'), os.listdir('./config/%s' % args.pid)))
     jar_files = map(lambda x: "lib/%s" % x, os.listdir('lib/'))
     commands = [
         'spark-submit',
@@ -23,7 +23,7 @@ else:
         '--executor-memory 4g',
         '--executor-cores 2',
         '--num-executors 4',
-        '--files %s' % (','.join(config_files + product_config_files)),
+       # '--files %s' % (','.join(config_files + product_config_files)),
         '--jars %s' % (','.join(jar_files)),
         'lib/us.pinguo.bigdata.test-1.0.jar',
         '%s-streaming.conf' % args.pid
